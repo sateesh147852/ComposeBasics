62d867b73865edf4cfaa7c518a7c9adf42fc1f2e
sateesh.chikkalagi <sateesh.chikkalagi@ril.com>
2024-09-23 20:35:26 +0530

    BottomNavigation Added

---
+++ app/src/main/java/com/composebasics/bottomNavigation/BottomNavigation.kt
@@ -0,0 +1,108 @@
+package com.composebasics.bottomNavigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomNavigationBar() {
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unSelectedIcon = Icons.Outlined.Home,
+            hasNews = false
+        ),
+        BottomNavigationItem(
+            title = "Chat",
+            selectedIcon = Icons.Filled.Email,
+            unSelectedIcon = Icons.Outlined.Email,
+            hasNews = false,
+            badgeCount = 45
+        ),
+        BottomNavigationItem(
+            title = "Settings",
+            selectedIcon = Icons.Filled.Settings,
+            unSelectedIcon = Icons.Outlined.Settings,
+            hasNews = true
+        )
+    )
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                items.forEachIndexed { index, bottomNavigationItem ->
+                    NavigationBarItem(
+                        selected = selectedItemIndex == index,
+                        onClick = {
+                            selectedItemIndex = index
+                        },
+                        label = {
+                                Text(text = bottomNavigationItem.title)
+                        },
+                        icon = {
+                            BadgedBox(badge = {
+                                if (bottomNavigationItem.badgeCount!=null){
+                                    Badge{
+                                        Text(text = bottomNavigationItem.badgeCount.toString())
+                                    }
+                                }else if (bottomNavigationItem.hasNews){
+                                    Badge()
+                                }
+                            }) {
+                                Icon(
+                                    imageVector = if (index == selectedItemIndex) bottomNavigationItem.selectedIcon else bottomNavigationItem.unSelectedIcon,
+                                    contentDescription = ""
+                                )
+                            }
+                        })
+                }
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+            items(100) {
+                Text(text = "Item $it", Modifier.padding(16.dp))
+            }
+        }
+    }
+}
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unSelectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
+)
