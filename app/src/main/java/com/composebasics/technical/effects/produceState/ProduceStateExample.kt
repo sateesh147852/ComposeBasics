25106a6a60d0daf9a7f57b1f8f8c50ea86edac84
sateesh.chikkalagi <sateesh.chikkalagi@ril.com>
2024-10-06 10:51:38 +0530

    ProduceStateExample added

--- app/src/main/java/com/composebasics/technical/effects/produceState/ProduceStateExample.kt
+++ app/src/main/java/com/composebasics/technical/effects/produceState/ProduceStateExample.kt
@@ -1,9 +1,20 @@
 package com.composebasics.technical.effects.produceState
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 
 @Composable
@@ -17,4 +28,31 @@ fun ProduceStateExample() {
     }
 
     Text(text = "${state.value}", style = MaterialTheme.typography.displayLarge)
+}
+
+@Composable
+fun ProduceStateExample2() {
+
+    val state = produceState(initialValue = 0) {
+        while (true) {
+            delay(25)
+            value = (value + 10) % 360
+        }
+    }
+
+    Box(
+        contentAlignment = Alignment.Center, modifier =
+        Modifier.fillMaxSize(1f)
+    ) {
+        Column {
+            Image(
+                imageVector = Icons.Default.Refresh,
+                contentDescription = "",
+                modifier = Modifier
+                    .size(50.dp)
+                    .rotate(state.value.toFloat())
+            )
+            Text(text = "Loading")
+        }
+    }
 }
